72ce6f6e8f10e10fb9e9b242cd40e128e50bdace
Ben Langmead <ben.langmead@gmail.com>
2012-11-26 15:00:05 -0500

    Elaborated comments and updated input/output message

--- src/rnawesome/splice.py
+++ src/rnawesome/splice.py
@@ -1,9 +1,7 @@
 #!/usr/bin/env python
 
 """
-align.py
-
-Alignment script, usually to be used in Hadoop apps.
+splice.py
 
 Wraps Bowtie.  Has features for (1) optionally extracting readlets,
 (2) optionally truncating reads or omitting mates, (3) hashing the read
@@ -35,9 +33,12 @@ args = parser.parse_args()
 
 binsz = partition.binSize(args)
 
+nout = 0
+
 def handleRead(rdid, ivals):
     ''' Given all the intervals where readlets from a given read
         aligned, compose a spliced alignment record. '''
+    global nout
     for k in ivals.iterkeys():
         for iv in iter(ivals[k]):
             st, en = iv.start, iv.end
@@ -46,27 +47,35 @@ def handleRead(rdid, ivals):
             # the offsets
             for pt in iter(partition.partition(k, st, en, binsz)):
                 print "%s\t%d\t%d\t%s\t%s" % (pt, st, en, k, sample.parseLab(rdid))
+                nout += 1
 
 last_rdid = "\t"
-nlines = 0
+ninp = 0
 rdid = None
 ivals = dict()
 
 for ln in sys.stdin:
+    # Parse next read
     ln = ln.rstrip()
     toks = ln.split('\t')
     rdid, fw, refid, refoff, seq, qual = toks[0], toks[1], toks[2], int(toks[3]), toks[4], toks[5]
+    # If we just moved on to readlets for the next read...
     if rdid != last_rdid:
-        handleRead(last_rdid, ivals)
+        if last_rdid != "\t":
+            # Process the intervals accumulated for the previous read
+            handleRead(last_rdid, ivals)
+        # Start from scratch for next read
         ivals = dict()
         last_rdid = rdid
+    # Add this interval to the flattened interval collection for current read
     if refid not in ivals:
         ivals[refid] = interval.FlatIntervals()
     ivals[refid].add(interval.Interval(refoff, refoff+len(seq)))
-    nlines += 1
+    ninp += 1
 
+# Process the intervals accumulated for the final read
 if last_rdid != "\t":
     handleRead(last_rdid, ivals)
 
 # Done
-print >>sys.stderr, "DONE with splice.py; processed %d lines" % nlines
+print >>sys.stderr, "DONE with splice.py; in/out = %d/%d" % (ninp, nout)
