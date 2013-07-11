b9d4d28cf1976a523707f87b839594a7c97ece0d
BenLangmead <ben.langmead@gmail.com>
2013-07-11 11:30:50 -0400

    Cleaned up aggr_path.py

--- src/rnawesome/aggr_path.py
+++ src/rnawesome/aggr_path.py
@@ -2,7 +2,8 @@
 aggr_path.py
 (after hmm.py)
 
-Collects the list of states from hmm and bins them into separate files according to permutation and sample
+Collects the list of states from hmm and bins them into separate files
+according to permutation and sample
 
 Tab-delimited output tuple columns:
  1. Dataset id (0 for test, >0 for permutations)
@@ -12,19 +13,19 @@ Tab-delimited output tuple columns:
  5. HMM state
 
 Other files:
- Sample files specified by permutation number
+ One file per permutation
  1. Reference ID
  2. Reference start position
  3. Reference end position
  4. HMM state
 
 '''
+
 import os
 import sys
 import argparse
 import time
-import pipes
-import subprocess, shlex
+import subprocess
 timeSt = time.clock()
 
 parser = argparse.ArgumentParser(description=\
@@ -49,61 +50,53 @@ parser.add_argument(\
 
 args = parser.parse_args()
 
-
-fname = "temp_file"        #temp file used to make a global file handle
-samp_out = open(fname,'w')
+fname, samp_out = None, None
 last_perm = "\t"                 #last permutation id
+ninp = 0
 
 def moveToHDFS(fin,fout):
     put_proc = subprocess.Popen([args.hadoop_exe, 'fs', '-put',fin, fout])
     put_proc.wait()
 
-
-
 for ln in sys.stdin:
+    ninp += 1
     ln = ln.rstrip()
     toks = ln.split('\t')
     assert len(toks)==5
     data_id, ref_id, ref_off, ref_len, hmm_st = toks[0], toks[1], int(toks[2]), int(toks[3]), int(toks[4])
     #print toks
-    if last_perm=="\t": 
-        samp_out.close()       
+    if last_perm=="\t":
+        if samp_out is not None:
+            samp_out.close()
         fname = "perm%s.bed"%(data_id)
         print>>sys.stderr,"ID:",data_id
         samp_out = open(fname,'w')
-        line = "%s\t%d\t%d\t%d\n"%(ref_id,ref_off,ref_off+ref_len,hmm_st)
-        samp_out.write(line)
-        last_perm=data_id
     elif last_perm!=data_id:
+        assert samp_out is not None
         samp_out.close()
-        bb_file = "perm%s.bb"%(last_perm)  if args.hadoop_exe!="" else "%s/perm%s.bb"%(args.out_dir,last_perm)
+        out_fname = "%s/perm%s.bb" % (args.out_dir, last_perm)
+        bb_file = "perm%s.bb" % (last_perm) if args.hadoop_exe != "" else out_fname
         bigbed_proc = subprocess.Popen([args.bigbed_exe, fname, args.chrom_sizes, bb_file])
         bigbed_proc.wait()
-        if args.hadoop_exe!="": #For hadoop mode - moves local file to HDFS
-            out_fname =  "%s/perm%s.bb"%(args.out_dir,last_perm)
+        if args.hadoop_exe != "": #For hadoop mode - moves local file to HDFS
             moveToHDFS(bb_file,out_fname)
+        os.remove(fname) # purge temporary bed file
         fname = "perm%s.bed"%(data_id)
         samp_out = open(fname,'w')
-
-        line = "%s\t%d\t%d\t%d\n"%(ref_id,ref_off,ref_off+ref_len,hmm_st)
-        samp_out.write(line)
-        last_perm=data_id
-    elif last_perm==data_id:
-        line = "%s\t%d\t%d\t%d\n"%(ref_id,ref_off,ref_off+ref_len,hmm_st)
-        samp_out.write(line)
+    line = "%s\t%d\t%d\t%d\n"%(ref_id,ref_off,ref_off+ref_len,hmm_st)
+    samp_out.write(line)
+    last_perm=data_id
 
 #Can't forget about last file
 samp_out.close()
-bb_file = "perm%s.bb"%(last_perm)
 out_fname =  "%s/perm%s.bb"%(args.out_dir,last_perm)
+bb_file = "perm%s.bb" % (last_perm) if args.hadoop_exe != "" else out_fname
 bigbed_proc = subprocess.Popen([args.bigbed_exe, fname, args.chrom_sizes, bb_file])
 bigbed_proc.wait()
 if args.hadoop_exe!="": #For hadoop mode - moves local file to HDFS
     moveToHDFS(bb_file,out_fname)
-
+os.remove(fname) # purge temporary bed file
 
 #Done
 timeEn = time.clock()
-print >>sys.stderr, "DONE with aggr_states.py; time=%0.3f secs" % (timeEn-timeSt)
-
-    
+print >>sys.stderr, "DONE with aggr_states.py; in = %d; time=%0.3f secs" % (ninp, timeEn-timeSt)
