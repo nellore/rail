69da5a09cc13ef9d5662a7cda2e995d33259409a
Jamie <jamie@jamie-Ideapad-Z570.(none)>
2013-07-28 17:58:31 -0400

    Modified simulator generator file

--- example/sim_splice/gen.sh
+++ example/sim_splice/gen.sh
@@ -1,6 +1,7 @@
 
-SIM=../../src/simulate/
+SIM=../../src/simulate
 ANNOTATIONS=../drosophila/Drosophila_melanogaster/UCSC/dm3/Annotation/Genes/genes.gtf
 GENOME=../drosophila/Drosophila_melanogaster/UCSC/dm3/Sequence/WholeGenomeFasta/genome.fa
-
-python $SIM/splice_sim.py --fasta=$GENOME --gtf=$ANNOTATIONS --output-prefix=fly --num-xscripts=2 --num-nucs=10000000
+DRIVERS=/media/jamie/3cd05818-b444-4592-81e3-e1fa9121c2c9/Documents/summer2013/workspace/tornado/drivers
+FASTAIDX=../drosophila/Drosophila_melanogaster/UCSC/dm3/Sequence/WholeGenomeFasta/genome.fa.fai
+python $SIM/splice_sim.py --fasta=$GENOME --gtf=$ANNOTATIONS --output-prefix=fly --num-xscripts=100 --num-nucs=10000000 --alternative-spliced=0 --stranded=1 --readmm_rate=0 --snp_rate=0 --indel_rate=0 --variants_file=$DRIVERS/variants.txt --chrsizes=$FASTAIDX
