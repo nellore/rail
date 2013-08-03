9f0b3a5b1de449b52330eeb021f6507e9a2339a9
BenLangmead <ben.langmead@gmail.com>
2013-08-03 09:13:11 -0400

    Adding work-in-progress script for installing PyPy on EMR machines

--- emr/bootstrap/install-pypy.sh
+++ emr/bootstrap/install-pypy.sh
@@ -10,3 +10,5 @@ sudo apt-get --yes install \
  libz-dev libbz2-dev libncurses-dev libexpat1-dev \
  libssl-dev libgc-dev python-sphinx python-greenlet || { echo "apt-get failed" ; exit 1 }
 
+# TODO: actually get PyPy.  Note that the pypy-c binary is not enough, you
+# also need to have all the supporting directories!
