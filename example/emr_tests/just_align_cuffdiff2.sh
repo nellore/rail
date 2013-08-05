2db0df68d20fdf552368610bda6fbe4f1da95bfe
BenLangmead <ben.langmead@gmail.com>
2013-08-04 23:28:15 -0400

    Fleshing out the simple cuffdiff2 example

--- example/emr_tests/just_align_cuffdiff2.sh
+++ example/emr_tests/just_align_cuffdiff2.sh
@@ -1,20 +1,27 @@
 #!/bin/sh
 
+d=`dirname $0`
+
 if [ -z "$TORNADO_HOME" ] ; then
 	echo "Set TORNADO_HOME first"
 	exit 1
 fi
 
-s3cmd del --recursive s3://langmead/tornado_cuffdiff2
+PROJ=cuffdiff2_small
+
+s3cmd del --recursive s3://langmead/tornado_${PROJ}
+
+s3cmd put ${d}/cuffdiff2_small.manifest s3://langmead/tornado_${PROJ}/manifest/
 
 python $TORNADO_HOME/src/driver/tornado.py \
 	--emr \
-	--input s3://tornado-emr/reads/cuffdiff2 \
-	--output s3://langmead/tornado_cuffdiff2 \
+	--manifest s3://langmead/tornado_${PROJ}/manifest/cuffdiff2_small.manifest \
+	--output s3://langmead/tornado_${PROJ}/output \
 	--reference s3://tornado-emr/refs/hg19_UCSC.tar.gz \
-	--just-align \
+	--start-with-preprocess \
+	--stop-after-align \
 	--instance-type c1.xlarge \
 	--instance-counts 1,0,0 \
 	$*
 
-echo "s3cmd del --recursive s3://langmead/tornado_cuffdiff2"
+echo "s3cmd del --recursive s3://langmead/tornado_${PROJ}"
