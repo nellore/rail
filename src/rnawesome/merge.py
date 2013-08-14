bb8b76c779dd7510cd6322000b46e3e519b02541
BenLangmead <ben.langmead@gmail.com>
2013-08-14 09:51:28 -0400

    Fixed major bugs in merge.py

--- src/rnawesome/merge.py
+++ src/rnawesome/merge.py
@@ -30,51 +30,84 @@ Tab-delimited output tuple columns (only column 5 is new):
 import sys
 import argparse
 import time
-timeSt = time.clock()
 
 parser = argparse.ArgumentParser(description=\
     'Merge reads that cover the same interval within a given partition.')
+
+parser.add_argument(\
+    '--partition-stats', action='store_const', const=True, help='Output statistics about bin sizes, time taken per bin, number of bins per reducer')
+parser.add_argument(\
+    '--profile', action='store_const', const=True, default=False, help='Profile the code')
+
 args = parser.parse_args()
 
-ninp, nout = 0, 0 # # lines input/output so far
-last_pt = "\t"    # last partition id
-last_refid = "\t" # last ref id
-last_st = -1      # last start pos
-last_en = -1      # last end pos
-cnts = dict()     # per-label counts for a given rdid
-ndigits = 12      #maxinum number of digits of start position number 
+ndigits = 12
+prefix = "o\t" if args.partition_stats else ""
 
-def flushCnts(pt, refid, st, en):
-    global nout
+def flushCnts(pt, refid, st, en, cnts):
+    nout = 0
     for k, v in cnts.iteritems():
         # k is group label, v is # times the interval occurred
         start = str(st)
-        assert len(start)<=ndigits
-        print "%s\t%012d\t%d\t%s\t%d\t%s" % (pt, st, en, refid, v, k)
+        assert len(start) <= ndigits
+        print "%s%s\t%012d\t%d\t%s\t%d\t%s" % (prefix, pt, st, en, refid, v, k)
         nout += 1
+    return nout
 
-for ln in sys.stdin:
-    ln = ln.rstrip()
-    toks = ln.split('\t')
-    assert len(toks) == 5, "Bad input:\n" + ln
-    pt, st, en, refid, lab = toks
-    st, en = int(st), int(en)
-    assert pt != last_pt or st >= last_st
-    if pt == last_pt and st == last_st and en == last_en:
-        # In a run of same-partition, same-end-point reads.  The
-        # previous may or may not have been for the same label.
-        cnts[lab] = cnts.get(lab, 0) + 1
-    else:
-        # Flush previous dict
-        if last_st >= 0:
-            flushCnts(last_pt, last_refid, last_st, last_en)
-        cnts = {lab:1}
-    last_pt, last_refid, last_st, last_en = pt, refid, st, en
-    ninp += 1
+def go():
+    ninp, nout = 0, 0 # # lines input/output so far
+    last_pt = "\t"    # last partition id
+    last_refid = "\t" # last ref id
+    last_st = -1      # last start pos
+    last_en = -1      # last end pos
+    cnts = dict()     # per-label counts for a given rdid
+    nbin = 1
+    timeSt = time.clock()
+    timeBinSt = timeSt
+    nInBin = 0
+    
+    for ln in sys.stdin:
+        ln = ln.rstrip()
+        toks = ln.split('\t')
+        assert len(toks) == 5, "Bad input:\n" + ln
+        pt, st, en, refid, lab = toks
+        st, en = int(st), int(en)
+        assert pt != last_pt or st >= last_st
+        if pt != last_pt:
+            if last_pt != '\t' and args.partition_stats:
+                timeBin = time.clock() - timeBinSt
+                print '\t'.join(['partstats', str(nInBin), str(timeBin)])
+                timeBinSt = time.clock()
+                nInBin = 0
+            nbin += 1
+        nInBin += 1
+        if pt == last_pt and st == last_st and en == last_en:
+            # In a run of same-partition, same-end-point reads.  The
+            # previous may or may not have been for the same label.
+            cnts[lab] = cnts.get(lab, 0) + 1
+        else:
+            # Flush previous dict
+            if last_st >= 0:
+                nout += flushCnts(last_pt, last_refid, last_st, last_en, cnts)
+            cnts = {lab:1}
+        last_pt, last_refid, last_st, last_en = pt, refid, st, en
+        ninp += 1
+    
+    if last_pt != '\t' and args.partition_stats:
+        timeBin = time.clock() - timeBinSt
+        print '\t'.join(['partstats', str(nInBin), str(timeBin)])
+    
+    if last_st >= 0:
+        nout += flushCnts(last_pt, last_refid, last_st, last_en, cnts)
+    
+    timeEn = time.clock()
+    
+    if args.partition_stats:
+        print '\t'.join(['reducerstats', str(nbin), str(ninp), str(nout), str(timeEn - timeSt)])
+    
+    print >>sys.stderr, "DONE with merge.py; in/out = %d/%d; time=%0.3f secs" % (ninp, nout, timeEn-timeSt)
 
-if last_st >= 0:
-    flushCnts(last_pt, last_refid, last_st, last_en)
-
-# Done
-timeEn = time.clock()
-print >>sys.stderr, "DONE with merge.py; in/out = %d/%d; time=%0.3f secs" % (ninp, nout, timeEn-timeSt)
+if args.profile:
+    import cProfile
+    cProfile.run('go()')
+else: go()
