8cae35e833fc5b3309e1017ac701a371d121f098
BenLangmead <ben.langmead@gmail.com>
2013-08-18 14:19:37 -0400

    More relaxed manifest parsing

--- src/manifest/manifest.py
+++ src/manifest/manifest.py
@@ -6,23 +6,26 @@ import os
 
 def addArgs(parser):
     parser.add_argument(\
-        '--manifest', metavar='FILE', type=str, required=False,
-         help='Manifest file')
+        '--manifest', metavar='PATH', type=str, required=False, help='Manifest file')
 
 def labels(args):
+    """ Parse the manifest file and return a list of all the labels in the
+        file """
     labs = []
     if not os.path.isfile(args.manifest):
         raise RuntimeError("No such --manifest file '%s'" % args.manifest)
-    fh = open(args.manifest)
-    for line in fh:
-        line = line.rstrip()
-        toks = line.split('\t')
-        if len(toks) == 3:
-            _, _, lab = toks
-            labs.append(lab)
-        elif len(toks) == 5:
-            _, _, _, _, lab = toks
-            labs.append(lab)
-        else:
-            raise RuntimeError("Wrong number of tokens in line: " + line)
+    with open(args.manifest, 'r') as fh:
+        for line in fh:
+            line = line.strip()
+            if len(line) == 0 or line[0] == '#':
+                continue
+            toks = line.split('\t')
+            if len(toks) == 3:
+                _, _, lab = toks
+                labs.append(lab)
+            elif len(toks) == 5:
+                _, _, _, _, lab = toks
+                labs.append(lab)
+            else:
+                raise RuntimeError("Wrong number of tokens in line: " + line)
     return labs
