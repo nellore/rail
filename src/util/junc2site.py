8e5959a63fe31780905ec8168b546de6ba973768
mortonjt <jamietmorton@gmail.com>
2013-08-18 18:57:13 -0400

    Added util scripts necessary for analyzing tophat output

---
+++ src/util/junc2site.py
@@ -0,0 +1,20 @@
+import os
+import sys
+import argparse
+import site
+import time
+
+#TODO: Make bedgraph output cleaner
+for ln in sys.stdin:
+    ln = ln.rstrip()
+    toks = ln.split('\t')
+    if len(toks)<11:
+        continue
+    seqid,junc_st,junc_end,depth,blocksz = toks[0],int(toks[1]),int(toks[2]),int(toks[4]),toks[10]
+    sizes = blocksz.split(',')
+    left_size,right_size = int(sizes[0]), int(sizes[1])
+    left, right = junc_st+left_size, junc_end-right_size
+    print "%s\t%d\t%d\t%d"%(seqid,left,left+1,depth)
+    print "%s\t%d\t%d\t%d"%(seqid,left+1,left+2,depth)
+    print "%s\t%d\t%d\t%d"%(seqid,right-2,right-1,depth)
+    print "%s\t%d\t%d\t%d"%(seqid,right-1,right,depth)
