336f3426c3f508ac91c28c0b031afb327ed7d4d9
BenLangmead <ben.langmead@gmail.com>
2013-08-21 17:48:25 -0400

    Added a couple more partition functions and fixed/added test cases

--- src/interval/partition.py
+++ src/interval/partition.py
@@ -31,6 +31,24 @@ def partition(refid, st, en, binSize):
     binid_en = int((en-1) / binSize)
     return [ (";".join([refid, str(i)]), i * binSize, (i+1) * binSize) for i in xrange(binid_st, binid_en+1) ]
 
+def partitionOverlaps(refid, st, en, binSize, fudge=0):
+    ''' Assign the interval refid:[st, en) to one or more partitions based on
+        which partitions are overlapped by the interval.  The interval is
+        (optionally) extended on both ends by some fudge factor before
+        overlapping. '''
+    binid_st = int((st - fudge    ) / binSize)
+    binid_en = int((en + fudge - 1) / binSize)
+    return [ (";".join([refid, str(i)]), i * binSize, (i+1) * binSize) for i in xrange(binid_st, binid_en+1) ]
+
+def partitionStartOverlaps(refid, st, en, binSize, fudge=0):
+    ''' Assign the interval refid:[st, en) to one or more partitions based on
+        which partitions are overlapped by the interval.  The interval is
+        (optionally) extended on both ends by some fudge factor before
+        overlapping. '''
+    binid_st = int((st - fudge) / binSize)
+    binid_en = int((st + fudge) / binSize)
+    return [ (";".join([refid, str(i)]), i * binSize, (i+1) * binSize) for i in xrange(binid_st, binid_en+1) ]
+
 def parse(st, binSz):
     ''' Parse a partition id. '''
     toks = st.split(";")
@@ -48,13 +66,33 @@ if __name__ == '__main__':
         def test1(self):
             pt = partition("blah", 27, 37, 10)
             self.assertEqual(2, len(pt))
-            refid, st, en = parse(pt[0], 10)
+            refid, st, en = parse(pt[0][0], 10)
             self.assertEqual("blah", refid)
             self.assertEqual(20, st)
             self.assertEqual(30, en)
-            refid, st, en = parse(pt[1], 10)
+            refid, st, en = parse(pt[1][0], 10)
             self.assertEqual("blah", refid)
             self.assertEqual(30, st)
             self.assertEqual(40, en)
-
+        
+        def test2(self):
+            ps = partitionOverlaps('p', 99, 100, 100, fudge=0)
+            self.assertEquals(1, len(ps))
+        
+        def test3(self):
+            ps = partitionOverlaps('p', 99, 100, 100, fudge=1)
+            self.assertEquals(2, len(ps))
+        
+        def test4(self):
+            ps = partitionStartOverlaps('p', 90, 95, 100, fudge=8)
+            self.assertEquals(1, len(ps))
+        
+        def test5(self):
+            ps = partitionStartOverlaps('p', 90, 95, 100, fudge=11)
+            self.assertEquals(2, len(ps))
+        
+        def test6(self):
+            ps = partitionOverlaps('p', 90, 95, 100, fudge=8)
+            self.assertEquals(2, len(ps))
+        
     unittest.main()
