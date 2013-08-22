d048a2b42d86a117c6f059240620281a6dd7d4af
mortonjt <jamietmorton@gmail.com>
2013-08-22 10:54:48 -0400

    An easier way to double check if pipeline is working via validate.sh

--- drivers/validate.sh
+++ drivers/validate.sh
@@ -13,22 +13,24 @@ COV=$SIM_SPLICE/fly.cov
 cat $SIM_SPLICE/intermediate/align_out.tsv | grep intron > $FLANKS   #really just the intron.py input
 
 # echo "Myrna Aligner validation"
-# python $CHECK/validate.py --xscripts-file=$XSCRIPTS --bed-file=$BEDSITES --radius=10 --refseq=$GENOME --sites-file=$SITES --flank-seqs=$FLANKS --coverage-file=$COV --window-radius=100 --false-negatives  | sed -e '/False/,/Region/!d' | paste -d " " - - | uniq > myrna_out
+sh run.sh
+python $CHECK/validate.py --xscripts-file=$XSCRIPTS --bed-file=$BEDSITES --radius=10 --refseq=$GENOME --sites-file=$SITES --flank-seqs=$FLANKS --coverage-file=$COV --window-radius=100
+#python $CHECK/validate.py --xscripts-file=$XSCRIPTS --bed-file=$BEDSITES --radius=10 --refseq=$GENOME --sites-file=$SITES --flank-seqs=$FLANKS --coverage-file=$COV --window-radius=100 --false-negatives  | sed -e '/False/,/Region/!d' | paste -d " " - - | uniq > myrna_out
 
 # python $CHECK/validate.py --xscripts-file=$XSCRIPTS --bed-file=$BEDSITES --radius=10 --refseq=$GENOME --sites-file=$SITES --flank-seqs=$FLANKS --coverage-file=$COV --window-radius=100 --false-negatives  > myrna_false_negatives
 
-python $CHECK/validate.py \
-    --xscripts-file=$XSCRIPTS \
-    --bed-file=$BEDSITES \
-    --radius=10 \
-    --refseq=$GENOME \
-    --sites-file=$SITES \
-    --flank-seqs=$FLANKS \
-    --coverage-file=$COV \
-    --window-radius=100 \
-    --false-positives  \
-    | sed -e '/False/,/Region/!d' | paste -d " " - - | sort | uniq | cut -d' ' -f 4 \
-    | perl -ne 'm/(\S+):(\d+)-(\d+)/; print "(refID=='\''$1'\'' or ( abs(st-$2)<=radius and abs(en-$3)<=radius)) or\n" '
+# python $CHECK/validate.py \
+#     --xscripts-file=$XSCRIPTS \
+#     --bed-file=$BEDSITES \
+#     --radius=10 \
+#     --refseq=$GENOME \
+#     --sites-file=$SITES \
+#     --flank-seqs=$FLANKS \
+#     --coverage-file=$COV \
+#     --window-radius=100 \
+#     --false-positives  \
+#     | sed -e '/False/,/Region/!d' | paste -d " " - - | sort | uniq | cut -d' ' -f 4 \
+#     | perl -ne 'm/(\S+):(\d+)-(\d+)/; print "(refID=='\''$1'\'' or ( abs(st-$2)<=radius and abs(en-$3)<=radius)) or\n" '
 
 # echo "Tophat validation"
 # python $CHECK/validate.py --xscripts-file=$XSCRIPTS --bed-file=$TOPHAT_SITES --radius=10 --refseq=$GENOME --sites-file=$SITES --flank-seqs=$FLANKS --coverage-file=$COV --window-radius=100 --false-negatives | sed -e '/False/,/Region/!d' | paste -d " " - - | uniq > tophat_out
