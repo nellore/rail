d1ce2deefe1115fffbf4f43c8090ccf5c9c795ba
BenLangmead <ben.langmead@gmail.com>
2013-08-25 22:31:57 -0400

    Updated simulation driver script, compatible with my earlier changes to sim_splice.py

--- example/sim_splice/gen.sh
+++ example/sim_splice/gen.sh
@@ -5,12 +5,14 @@ GENOME=../drosophila/Drosophila_melanogaster/UCSC/dm3/Sequence/WholeGenomeFasta/
 DRIVERS=$PWD/../../drivers
 FASTAIDX=../drosophila/Drosophila_melanogaster/UCSC/dm3/Sequence/WholeGenomeFasta/genome.fa.fai
 ANNOT_SITES=fly.genes
-python $SIM/splice_sim.py --fasta=$GENOME --gtf=$ANNOTATIONS --output-prefix=$PWD/fly --num-xscripts=100 --num-nucs=10000000 --alternative-spliced=0 --stranded=1 --readmm_rate=0.01 --snp_rate=0 --indel_rate=0 --chrsizes=$FASTAIDX --output-annotations=$ANNOT_SITES --paired-end=1
+python $SIM/splice_sim.py --fasta=$GENOME --gtf=$ANNOTATIONS --output-prefix=$PWD/fly --num-xscripts=100 --num-nucs=10000000 --alternative-spliced=0 --stranded=1 --readmm_rate=0.01 --snp_rate=0 --indel_rate=0 --chrsizes=$FASTAIDX --output-annotations=$ANNOT_SITES --paired-end
 
-rm fly.genes.gtf
+# BTL: Not sure if the following is necessary but it's very slow!
+
+#rm fly.genes.gtf
 #IFS=$'\n';
-for line in `cat $ANNOT_SITES`
-do
-    echo $line
-    cat $ANNOTATIONS | grep $line | grep exon >> fly.genes.gtf
-done
+#for line in `cat $ANNOT_SITES`
+#do
+#    echo $line
+#    cat $ANNOTATIONS | grep $line | grep exon >> fly.genes.gtf
+#done
