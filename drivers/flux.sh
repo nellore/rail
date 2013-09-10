d62f013986badf532de999b28084936e2354d8b0
Jamie <jamietmorton@gmail.com>
2013-09-10 13:38:34 -0400

    Potentially fixed sed problem

--- drivers/flux.sh
+++ drivers/flux.sh
@@ -1,5 +1,6 @@
 #./flux-simulator/bin/flux-simulator -p parameters/sailfish.par
 #SIM_OUT=simulation_output
+FLUX_HOME=$TORNADO_HOME/tools/flux_sim/flux-simulator
 python $TORNADO_HOME/src/simulate/flux.py \
     --output-dir=$SIM_HOME \
     --flux-path=$FLUX_HOME \
@@ -17,5 +18,5 @@ for FASTQ in $SIM_HOME/*.fastq
 do
     name=`basename $FASTQ .fastq`
     echo $name
-    sed -i '1~4 s/\@/\@'$name'./' $FASTQ
+    sed -i -e '1~4 s/\@/\@'$name'./' $FASTQ
 done
