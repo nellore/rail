1c960cabee4cbfc1dd5d1ebabab70274a034ef28
BenLangmead <ben.langmead@gmail.com>
2013-09-15 16:22:33 -0400

    Fix to kenttools download

--- emr/bootstrap/install-kenttools.sh
+++ emr/bootstrap/install-kenttools.sh
@@ -16,6 +16,13 @@ fi
 
 # wget the needed files
 for i in bedToBigBed bigBedToBed wigToBigWig bigWigToWig ; do
-	wget -S -T 10 -t 5 http://hgdownload.cse.ucsc.edu/admin/exe/linux.x86_64/$i || { echo 'wget failed' ; exit 1; }
+	# Note: the binaries in the linux.x86_64 directory stopped working as of
+	# summer 2013.  I hope the linux.x86_64.v287 directory sticks around
+	# forever!
+	wget -S -T 10 -t 5 http://hgdownload.cse.ucsc.edu/admin/exe/linux.x86_64.v287/$i || { echo 'wget failed' ; exit 1; }
 	chmod a+x $i
+	if [ ! -x $i ] ; then
+		echo "$1/$i does not exist or isn't executable"
+		exit 1
+	fi
 done
