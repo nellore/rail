72f899355559d14762f5dd627222b7ca2aa4075e
BenLangmead <ben.langmead@gmail.com>
2013-09-26 23:12:04 -0400

    More useful defaults

--- example/dmel_flux/local.sh
+++ example/dmel_flux/local.sh
@@ -8,8 +8,10 @@ python $TORNADO_HOME/src/driver/tornado.py \
 	--start-with-align --no-differential \
 	--manifest dmel_flux.manifest \
 	--input preprocessed_reads \
+	--intermediate intermediate \
 	--output local_out \
 	--reference $IGENOMES_HOME/Drosophila_melanogaster/UCSC/dm3 \
 	--igenomes \
 	--num-processes=6 \
+	--keep-all \
 	$*
