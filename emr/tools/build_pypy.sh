e844592bf32c872ccdd78af83df5e02ef062884d
BenLangmead <ben.langmead@gmail.com>
2013-10-08 09:43:57 -0400

    updated comment

--- emr/tools/build_pypy.sh
+++ emr/tools/build_pypy.sh
@@ -5,6 +5,11 @@
 # Download pypy source and build a binary.  This is presumably something we do
 # on an EMR machine so that there's a working version built on the appropriate
 # OS.
+#
+# BTL: This took a very long time when I last tried it, and I'm not confident
+# that what gets produced is portable.  If anyone discovers a good way to get
+# pypy up and running on a new EMR instance quickly (such that we could do so
+# in a bootstrap action) please let me know.
 
 # Got this list from http://doc.pypy.org/en/latest/getting-started-python.html
 sudo apt-get --yes install \
