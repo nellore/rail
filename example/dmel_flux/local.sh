c721c47bb68b60ab5ee3b8617534074b36a2d90b
BenLangmead <ben.langmead@gmail.com>
2013-12-27 22:07:40 -0500

    Made intenting consistent

--- example/dmel_flux/local.sh
+++ example/dmel_flux/local.sh
@@ -4,15 +4,15 @@
 [ -z "$IGENOMES_HOME" ] && echo "Set IGENOMES_HOME" && exit 1
 
 python $RAIL_HOME/src/driver/rail-rna.py \
-	--local \
-	--start-with-align --no-differential \
-	--manifest dmel_flux.manifest \
-	--input preprocessed_reads \
-	--intermediate intermediate \
-	--output local_out \
-        --reference $IGENOMES_HOME/Drosophila_melanogaster/UCSC/dm3 \
-        --igenomes \
-        --stop-after-align \
-        --num-processes=6 \
-	--keep-all \
-	$*
+    --local \
+    --start-with-align --no-differential \
+    --manifest dmel_flux.manifest \
+    --input preprocessed_reads \
+    --intermediate intermediate \
+    --output local_out \
+    --reference $IGENOMES_HOME/Drosophila_melanogaster/UCSC/dm3 \
+    --igenomes \
+    --stop-after-align \
+    --num-processes=6 \
+    --keep-all \
+    $*
