09e365da3c953295caa823e51a5bce27169ccae0
Alyssa Frazee <acfrazee@gmail.com>
2014-02-08 16:13:51 -0500

    updated pop_data to include unique sample IDs matching the ones the authors gave us

--- data/GEUVADIS/get_populations.py
+++ data/GEUVADIS/get_populations.py
@@ -2,17 +2,18 @@
 # AF 23 Oct 2013
 
 #### download ENA accesson numbers 
-import urllib
 import urllib2
-
-ena_info = urllib2.urlopen("http://www.ebi.ac.uk/ena/data/warehouse/search?query=%22secondary_study_accession=%22ERP001942%22%22&result=read_run&limit=667&length=667&offset=0&display=report&fields=study_accession,secondary_study_accession,sample_accession_list,experiment_accession,run_accession,scientific_name,instrument_model,library_layout,fastq_ftp,fastq_galaxy,submitted_ftp,submitted_galaxy,col_taxonomy")
+ena_info = urllib2.urlopen("http://www.ebi.ac.uk/ena/data/warehouse/filereport?accession=ERP001942&result=read_run&fields=study_accession,secondary_study_accession,sample_accession,secondary_sample_accession,experiment_accession,run_accession,scientific_name,instrument_model,library_layout,fastq_ftp,fastq_galaxy,submitted_ftp,submitted_galaxy,col_tax_id,col_scientific_name")
+#ena_info = urllib2.urlopen("http://www.ebi.ac.uk/ena/data/warehouse/search?query=%22secondary_study_accession=%22ERP001942%22%22&result=read_run&limit=667&length=667&offset=0&display=report&fields=study_accession,secondary_study_accession,sample_accession_list,experiment_accession,run_accession,scientific_name,instrument_model,library_layout,fastq_ftp,fastq_galaxy,submitted_ftp,submitted_galaxy,col_taxonomy") ### OUTDATED as of 2/8/14
 dat = ena_info.readlines()
-table_rows = [x.split('\t') for x in dat]
-sample_ftps = [x[10] for x in table_rows]
-len(set(sample_ftps)) #668, too many
+table_rows = [x.split('\t') for x in dat][1:] #strip off header row
+col_labels = dat[0].split('\t')
+ftpind = col_labels.index('submitted_ftp') #11
+sample_ftps = [x[ftpind] for x in table_rows]
+len(set(sample_ftps)) #667
 numinfront = len("ftp.sra.ebi.ac.uk/vol1/ERA169/ERA169774/fastq/")
 hapmapids = [x[numinfront:][:7] for x in sample_ftps]
-len(set(hapmapids)) #464 unique hapmap samples (-1 for col name)
+len(set(hapmapids)) #464 unique hapmap samples 
 
 ## note: from paper supplement:
 ## 5 samples sequenced in replicate at each of 7 labs (so 8x total)
@@ -30,13 +31,27 @@ for entry in pdrows[1:]:
     lookup[entry[idcol]] = entry[popcol]
 
 out_table = 'pop_data.txt'
+errind = col_labels.index('run_accession')
 with open(out_table, 'w') as f:
     f.write('run_id\thapmap_id\tpopulation\n')
-    tr = table_rows[1:]
     pos = 0
-    for h in hapmapids[1:]:
-        f.write(tr[pos][4]+'\t'+h+'\t'+lookup[h]+'\n')
+    for h in hapmapids:
+        f.write(table_rows[pos][errind]+'\t'+h+'\t'+lookup[h]+'\n')
         pos += 1
+
+# running a diff on the output of this and the old copy of pop_data showed that everything from before is still right - AWESOME
+
+# now add identifiers to this table (to look up in the table from the geuvadis authors)
+ids = [x.split(';')[0].split('/')[-1][:-11] for x in sample_ftps]
+with open(out_table, 'w') as f:
+    f.write('sample_id\trun_id\thapmap_id\tpopulation\n')
+    pos = 0
+    for h in hapmapids:
+        f.write(ids[pos]+'\t'+table_rows[pos][errind]+'\t'+h+'\t'+lookup[h]+'\n')
+        pos += 1
+
+
+
 
 
 
