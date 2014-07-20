270c775a5a1782263a54e274432f90f140fc1597
buci <anellore@gmail.com>
2014-07-20 15:22:26 -0400

    prettifies TPM plot

--- eval/preprocess_all_of_GEUVADIS.sh
+++ eval/preprocess_all_of_GEUVADIS.sh
@@ -4,6 +4,6 @@
 RAILHOME=~/railclones/rail
 
 # Set output directory here -- must be on S3!
-OUTPUT=s3://rail-experiments/GEU
+OUTPUT=s3://rail-experiments/GEUV
 
-python $RAILHOME/src prep elastic -m GEUVADIS_all_samples.manifest -c 20 --core-instance-bid-price 0.11 --master-instance-bid-price 0.11 -o $OUTPUT --ec2-key-name rail
+python $RAILHOME/src prep elastic -m GEUVADIS_all_samples.manifest -c 20 --core-instance-bid-price 0.11 --master-instance-bid-price 0.11 -o $OUTPUT --ec2-key-name rail --do-not-check-manifest
