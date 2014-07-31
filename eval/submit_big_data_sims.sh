9edcdbb265f33a5011a532c3d6d7cfd2a9755576
buci <anellore@gmail.com>
2014-07-30 22:43:48 -0400

    tweaks script for submitting biorep sim

--- eval/submit_big_data_sims.sh
+++ eval/submit_big_data_sims.sh
@@ -7,4 +7,4 @@ RAILHOME=~/railclones/rail
 OUTBASENAME=s3://rail-experiments/geuvadis_sim
 
 # Run Rail-RNA on 20 simulated "bioreps", pooling info across them
-python $RAILHOME/src go elastic -a hg19 -m $RAILHOME/eval/geuvadis_sim.manifest -o ${OUTBASENAME}_allbioreps_pres.nobin -c 40 --core-instance-bid-price 0.11 --master-instance-bid-price 0.11 --master-instance-type c3.2xlarge --core-instance-type c3.2xlarge -f
+python $RAILHOME/src go elastic -a hg19 -m $RAILHOME/eval/geuvadis_sim.manifest -o ${OUTBASENAME}_biorep_sim -c 40 --core-instance-bid-price 0.11 --master-instance-bid-price 0.11 --master-instance-type c3.2xlarge --core-instance-type c3.2xlarge -f
