54acb027a7256130b9536ec7cc07501f1895c1bd
Abhinav Nellore <anellore@gmail.com>
2015-03-11 09:55:22 -0400

    ups max task attempts to 10 in 112 preprocess job

--- eval/preprocess_112_random_GEUVADIS.sh
+++ eval/preprocess_112_random_GEUVADIS.sh
@@ -6,4 +6,4 @@ RAILHOME=~/rail
 # Set output directory here -- must be on S3!
 OUTPUT=s3://rail-results/geuv112_ami_3_4_0d
 
-python $RAILHOME/src prep elastic -m $RAILHOME/eval/GEUVADIS_112_staged.manifest -c 20 --core-instance-type c3.2xlarge --master-instance-type c3.2xlarge --core-instance-bid-price 0.11 --master-instance-bid-price 0.11 -o $OUTPUT --ec2-key-name rail2 --do-not-check-manifest
+python $RAILHOME/src prep elastic -m $RAILHOME/eval/GEUVADIS_112_staged.manifest -c 20 --max-task-attempts 10 --core-instance-type c3.2xlarge --master-instance-type c3.2xlarge --core-instance-bid-price 0.11 --master-instance-bid-price 0.11 -o $OUTPUT --ec2-key-name rail2 --do-not-check-manifest
