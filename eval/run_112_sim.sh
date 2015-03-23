10dc1a1b527605fd2ed946880044015e5e9e3996
Abhinav Nellore <anellore@gmail.com>
2015-03-23 12:33:57 -0400

    adds table of scaling experiment results

---
+++ eval/run_112_sim.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+## A script that:
+## (1) Compresses the FASTQs and stages them on S3
+## (2) Creates a manifest file for the FASTQs
+## (3) Runs Rail-RNA on the 112 datasets on Elastic MapReduce
+
