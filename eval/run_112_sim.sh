50da1196160023e93eb90f1e9d179e3cb3d769ea
Abhinav Nellore <anellore@gmail.com>
2015-03-24 10:19:12 -0400

    adds script for aligning 112 simulated samples

--- eval/run_112_sim.sh
+++ eval/run_112_sim.sh
@@ -3,4 +3,17 @@
 ## (1) Compresses the FASTQs and stages them on S3
 ## (2) Creates a manifest file for the FASTQs
 ## (3) Runs Rail-RNA on the 112 datasets on Elastic MapReduce
-
+## First command-line parameter: directory with fastqs
+## Second command-line parameter: where on S3 to stage fastq.gzs
+## Third command-line parameter: path to manifest file to write
+## Requires s3cmd
+FASTQDIR=$1
+S3STAGED=$2
+S3DEST=$3
+MANIFEST=$4
+cd $FASTQDIR
+ls *.fastq | python -c "import sys; for line in sys.stdin:
+    sample_name = list(line.strip().rpartition('_')[0].partition('-'))
+    sample_name[0] = sample_name[0] + '_sim'
+    print '\t'.join([${S3STAGED}/line.strip(), '0', ''.join(sample_name)])"
+#for i in *.fastq; do cat $i | gzip | s3cmd put - $S3DEST/$i.gz; done
--- package/package_rail.sh
+++ package/package_rail.sh
@@ -5,18 +5,14 @@
 # Make a tarball out of all the Rail scripts 
 #
 
-cd $(dirname "${BASH_SOURCE[0]}")
-VER=`cat ../src/rna/utils/version.py | grep version_number | cut -f 2 -d "'"`
-ARNAME=rail-rna-${VER}.tar.gz
-
-# Nothing to make now; just copy jars
-# make -C hadoop
-
-# Make lib directory for the hacked hadoop-streaming jar
-# rm -rf lib
-# mkdir -p lib
+ORIGINAL=$(pwd)
+PACKDIR=$(dirname "${BASH_SOURCE[0]}")
+cd $PACKDIR
 cd ..
-tar czvf ${ARNAME} --exclude '*.pyc' --exclude '*.tar.gz' src lib
-
-echo "This should be executed from Rail's root directory!"
-echo "s3cmd put --acl-public ${ARNAME} s3://rail-emr/bin/"
+VER=`cat src/version.py | grep version_number | cut -f 2 -d "'"`
+ARNAME=rail-rna-${VER}.tar.gz
+rm -rf $ARNAME
+tar czvf ${ARNAME} --exclude '*.pyc' --exclude '*.tar.gz' --exclude '.DS_Store' src lib
+mv $ARNAME $PACKDIR/$ARNAME
+echo "Run this to upload to S3."
+echo "s3cmd put --acl-public ${PACKDIR}/${ARNAME} s3://rail-emr/bin/"
