a7a40928a1113d112da21e2adf6263c105257de6
Abhinav Nellore <anellore@gmail.com>
2015-03-24 17:57:21 -0400

    fixes unicode bug

--- eval/run_112_sim.sh
+++ eval/run_112_sim.sh
@@ -9,7 +9,7 @@
 ## $4: path to Rail source directory (wherever rail/src is)
 ## $5: path to manifest file to write (taken in our experiments to be eval/GEUVADIS_112_sim.manifest)
 ## Requires AWS CLI
-## Full command we ran was sh run_112_sim.sh /scratch0/langmead-fs1/geuvadis_sims_for_paper s3://rail-results/geuv112sim /scratch0/langmead-fs1/rail/src /scratch0/langmead-fs1/rail/src/eval/GEUVADIS_112_sim.manifest
+## Full command we ran was sh run_112_sim.sh /scratch0/langmead-fs1/geuvadis_sims_for_paper s3://rail-results/geuv112sim s3://rail-results/geuv112sim.out /scratch0/langmead-fs1/rail/src /scratch0/langmead-fs1/rail/src/eval/GEUVADIS_112_sim.manifest
 FASTQDIR=$1
 S3STAGED=$2
 S3DEST=$3
@@ -22,7 +22,7 @@ for line in sys.stdin:
     sample_name = list(line.strip().rpartition('_')[0].partition('-'))
     sample_name[0] = sample_name[0] + '_sim'
     print '\t'.join(['${S3STAGED}/' + line.strip(), '0', ''.join(sample_name)])" >$MANIFEST
-#for i in $(find . -name \*.fastq ! -name \*right\*.fastq ! -name \*left\*.fastq | cut -c3-); do aws s3 cp $i $S3STAGED/$i; done
+for i in $(find . -name \*.fastq ! -name \*right\*.fastq ! -name \*left\*.fastq | cut -c3-); do aws s3 cp $i $S3STAGED/$i; done
 # Submit job to EMR
 python $RAILSRC go elastic -c 40 -a hg19 -m $MANIFEST -o $S3DEST --core-instance-bid-price 0.11 --master-instance-bid-price 0.11
 cd $ORIGINAL
