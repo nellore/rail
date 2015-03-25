39557ea8806eaa4399bf8f7b178ac1069049d2cc
Abhinav Nellore <anellore@gmail.com>
2015-03-24 20:20:28 -0400

    reveals stderr output

--- eval/run_all_small_data_sims_locally.sh
+++ eval/run_all_small_data_sims_locally.sh
@@ -41,7 +41,7 @@ SAMPLE=$SAMPLE1
 echo 'Running Rail-RNA on sample '${SAMPLE}'...'
 # Write manifest file
 echo -e $DATADIR/${SAMPLE}_sim.fastq'\t0\t'${SAMPLE} >$MAINOUTPUT/${SAMPLE}.manifest
-$RAILRNA go local -p $CORES -m $MAINOUTPUT/${SAMPLE}.manifest -o $OUTPUT/rail --log $OUTPUT/rail.log -x $BOWTIE1IDX,$BOWTIE2IDX -f >/dev/null 2>&1
+$RAILRNA go local -p $CORES -m $MAINOUTPUT/${SAMPLE}.manifest -o $OUTPUT/rail --log $OUTPUT/rail.log -x $BOWTIE1IDX,$BOWTIE2IDX -f
 echo 'Computing precision and recall...'
 (for i in $OUTPUT/rail/alignments/*.bam; do $SAMTOOLS view $i; done | $PYTHON $RAILHOME/eval/spliced_read_recovery_performance.py -t $DATADIR/${SAMPLE}_sim.bed >$OUTPUT/rail/$PERFORMANCE 2>$OUTPUT/rail/${PERFORMANCE}_summary) &
 (for i in $OUTPUT/rail/alignments/*.bam; do $SAMTOOLS view $i; done | $PYTHON $RAILHOME/eval/intron_recovery_performance.py -t $DATADIR/${SAMPLE}_sim.bed >$OUTPUT/rail/${PERFORMANCE}_intron_recovery_summary) &
