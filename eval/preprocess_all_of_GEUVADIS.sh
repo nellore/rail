389e5c8f3d8211764b206823de3e75f125017341
Abhinav Nellore <anellore@gmail.com>
2015-03-31 15:14:41 -0400

    changes region in prep job

--- eval/preprocess_all_of_GEUVADIS.sh
+++ eval/preprocess_all_of_GEUVADIS.sh
@@ -7,4 +7,4 @@ RAILHOME=~/rail
 # Set output directory here -- must be on S3!
 OUTPUT=s3://rail-west-2/geuvprepped
 
-python $RAILHOME/src prep elastic -m GEUVADIS_all_descriptive_staged.manifest -c 84 --core-instance-bid-price 0.11 --master-instance-bid-price 0.11 --core-instance-type c3.2xlarge --master-instance-type c3.2xlarge -o $OUTPUT --ec2-key-name westernpacificrail --no-consistent-view -f --do-not-check-manifest
+python $RAILHOME/src prep elastic -m GEUVADIS_all_descriptive_staged.manifest -c 84 --core-instance-bid-price 0.11 --master-instance-bid-price 0.11 --core-instance-type c3.2xlarge --master-instance-type c3.2xlarge -o $OUTPUT --ec2-key-name westernpacificrail --no-consistent-view -f --region us-west-2 --do-not-check-manifest
