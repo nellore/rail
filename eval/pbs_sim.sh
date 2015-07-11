44783edfba2ad2a1af55d2dfba598fe33008e789
Abhinav Nellore <anellore@gmail.com>
2015-07-11 08:29:20 -0400

    gets pbs script generation right

--- eval/pbs_sim.sh
+++ eval/pbs_sim.sh
@@ -5,10 +5,4 @@ DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 cd $DIR
 mkdir -p pbs_scripts
 cd pbs_scripts
-for i in $(python ../create_single_sample_sim_commands.py); do cat <(echo -e "
-#!/bin/tcsh
-#PBS -q batch
-#PBS -l walltime=48:00
-#PBS -l nodes=1:ppn=8
-#PBS -j n
-") <(echo $i); done
+i=0; IFS=$'\n'; for j in $(python ../create_single_sample_sim_commands.py); do python -c "print '#!/bin/bash\n#PBS -q batch\n#PBS -l walltime=48:00\n#PBS -l nodes=1:ppn=8\n#PBS -j n\n#PBS -m abe\n#PBS -M anellore@gmail.com\n$j'" >pbs_$i; let i=i+1; done
