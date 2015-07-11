0b2d18fd0f1d59a05c1d7a3f38572361f135922c
Abhinav Nellore <anellore@gmail.com>
2015-07-11 11:15:52 -0400

    does not do subjunc on cluster

--- eval/pbs_sim.sh
+++ eval/pbs_sim.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 # Script run on Johns Hopkins cluster to generate PBS scripts for running simulations
 # May have to be adjusted by the user to suit other clusters
-# STAR simulations were not run on cluster because of memory requirement; they were run separately on a high-memory node
+# STAR/subjunc simulations were not run on cluster because of memory requirement; they were run separately on a high-memory node
 DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 cd $DIR
 mkdir -p pbs_scripts
 cd pbs_scripts
-i=0; IFS=$'\n'; for j in $(python ../create_single_sample_sim_commands.py --aligners tophat rail hisat subjunc); do python -c "print '#!/bin/bash\n#PBS -q batch\n#PBS -l walltime=48:00\n#PBS -l nodes=1:ppn=8\n#PBS -j n\n#PBS -m abe\n#PBS -M anellore@gmail.com\n$j'" >pbs_$i; let i=i+1; done
+i=0; IFS=$'\n'; for j in $(python ../create_single_sample_sim_commands.py --aligners tophat rail hisat); do python -c "print '#!/bin/bash\n#PBS -q batch\n#PBS -l walltime=48:00\n#PBS -l nodes=1:ppn=8\n#PBS -j n\n#PBS -m abe\n#PBS -M anellore@gmail.com\n$j'" >pbs_$i; let i=i+1; done
