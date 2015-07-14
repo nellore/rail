4563037a6755f6d6cc7627cd2df453f1c1dcddb9
Abhinav Nellore <anellore@gmail.com>
2015-07-13 23:52:49 -0400

    updates all of geuvadis scripts

--- eval/preprocess_all_of_GEUVADIS.sh
+++ eval/preprocess_all_of_GEUVADIS.sh
@@ -1,10 +1,8 @@
-
 #!/usr/bin/env bash
-
-# Set location of Rail repo here
-RAILHOME=~/rail
+# Use Rail-RNA v0.1.8 to reproduce results in v2 of Rail paper
 
 # Set output directory here -- must be on S3!
-OUTPUT=s3://rail-eu-west-1/geuvprepped
+OUTPUT=s3://rail-eu-west-1/geuv667prepped
 
-python $RAILHOME/src prep elastic -m GEUVADIS_all_descriptive_staged.manifest -c 1 -o $OUTPUT -f --region eu-west-1 --do-not-check-manifest --ec2-key-name raileuw1
+# Change --ec2-key-name as appropriate below or simply delete it if you don't intend to SSH to the EMR cluster
+rail-rna prep elastic -m GEUVADIS_all_descriptive_revised.manifest -c 20 -o $OUTPUT --core-instance-bid-price 0.13 --master-instance-bid-price 0.13 -f --region eu-west-1 --do-not-check-manifest --ec2-key-name raileuw1
--- eval/submit_all_of_geuvadis_job.sh
+++ eval/submit_all_of_geuvadis_job.sh
@@ -1,8 +1,7 @@
 #!/usr/bin/env bash
-# Set location of Rail repo here
-RAILHOME=~/rail
 
 # Set input/output bucket here -- must be on S3!
 BUCKET=s3://rail-eu-west-1
 
-python $RAILHOME/src align elastic -i s3://rail-eu-west-1/geuvprepped -a hg19 -o $BUCKET/GEUVADIS_v4.04.2015 -c 60 -m $RAILHOME/eval/GEUVADIS_all_descriptive_staged.manifest --master-instance-type c3.8xlarge --core-instance-type c3.8xlarge --master-instance-bid-price 0.35 --core-instance-bid-price 0.35 --ec2-key-name raileuwest1 --no-consistent-view -f --max-task-attempts 10 --region eu-west-1
+# Chance --ec2-key-name or remove it below
+rail-rna align elastic -i $BUCKET/geuv667prepped -a hg19 -o $BUCKET/GEUVADIS_v4.04.2015 -c 60 -m $RAILHOME/eval/GEUVADIS_all_descriptive_revised.manifest --master-instance-type c3.8xlarge --core-instance-type c3.8xlarge --master-instance-bid-price 0.35 --core-instance-bid-price 0.35 --ec2-key-name raileuw1 -f --max-task-attempts 10 --region eu-west-1
