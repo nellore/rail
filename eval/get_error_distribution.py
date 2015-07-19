c52d5b1350f0d642c5911d08a0e678aea5cc66c7
Abhinav Nellore <anellore@gmail.com>
2015-07-19 08:16:17 -0400

    removes assets from master

---
+++ eval/get_error_distribution.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python
+"""
+get_error_distribution.py
+
+Gets error distribution of Flux BED/FASTQ. Considers only reads from the FASTQ
+with the length specified at the command line.
+"""
