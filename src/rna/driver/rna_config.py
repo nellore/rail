bbaf1cf52e999fd92c7ed411b28cd68aeff517ff
abhinav <anellore@gmail.com>
2015-07-20 10:33:18 -0400

    fixes minor bug where bowtie2-build executable path was not specified

--- src/rna/driver/rna_config.py
+++ src/rna/driver/rna_config.py
@@ -4171,9 +4171,11 @@ class RailRnaAlign(object):
             {
                 'name' : 'Align reads to transcriptome elements',
                 'reducer' : ('realign_reads.py --bowtie2-exe={0} '
-                             '--gzip-level {1} --count-multiplier {2} '
-                             '--tie-margin {3} {4} {5} {6} -- {7}').format(
+                             '--bowtie2-build-exe={1} '
+                             '--gzip-level {2} --count-multiplier {3} '
+                             '--tie-margin {4} {5} {6} {7} -- {8}').format(
                                             base.bowtie2_exe,
+                                            base.bowtie2_build_exe,
                                             base.gzip_level
                                             if 'gzip_level' in
                                             dir(base) else 3,
