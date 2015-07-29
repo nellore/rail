b009076351903db696fbf4810d8b84515cd0927d
Abhinav Nellore <anellore@gmail.com>
2015-07-29 14:05:10 -0400

    fixes 112 sim script to accommmodate paired-end runs

--- eval/run_112_sim.sh
+++ eval/run_112_sim.sh
@@ -9,19 +9,27 @@
 ## $4: path to manifest file to write (taken in our experiments to be eval/GEUVADIS_112_sim.manifest)
 ## Requires AWS CLI
 ## Full command we ran was sh run_112_sim.sh /scratch2/langmead-fs1/geuvadis_sims_for_paper_v2 s3://rail-eu-west-1/geuv112sim_v2 s3://rail-eu-west-1/geuv112sim_v2.out /scratch0/langmead-fs1/rail/eval/GEUVADIS_112_sim.manifest
-## Use Rail-RNA v0.1.9
+## Make sure Rail-RNA v0.1.9 is used to reproduce our results
 FASTQDIR=$1
 S3STAGED=$2
 S3DEST=$3
 MANIFEST=$4
 cd $FASTQDIR
+for fastq in $(find . -name \*.fastq ! -name \*right\*.fastq ! -name \*left\*.fastq | cut -c3-)
+do
+	leftfile=$(echo ${fastq} | rev | cut -c 7- | rev)_left.fastq
+	rightfile=$(echo ${fastq} | rev | cut -c 7- | rev)_right.fastq
+	awk '(NR-1) % 8 < 4' $fastq >$leftfile
+	awk '(NR-1) % 8 >= 4' $fastq >$rightfile
+done
 find . -name \*.fastq ! -name \*right\*.fastq ! -name \*left\*.fastq | cut -c3- | python -c "import sys
 for line in sys.stdin:
     sample_name = list(line.strip().rpartition('_')[0].partition('-'))
     sample_name[0] = sample_name[0] + '_sim'
-    print '\t'.join(['${S3STAGED}/' + line.strip(), '0', ''.join(sample_name)])" >$MANIFEST
+    sample_root = line.strip().rpartition('.')[0]
+    print '\t'.join(['${S3STAGED}/' + sample_root + '_left.fastq', '0', '${S3STAGED}/' + sample_root + '_right.fastq', '0', ''.join(sample_name)])" >$MANIFEST
 for i in $(find . -name \*.fastq ! -name \*right\*.fastq ! -name \*left\*.fastq | cut -c3-); do aws s3 cp $i $S3STAGED/$i; done
-# Submit jobs to EMR
-rail-rna go elastic -c 40 -a hg19 -m $MANIFEST -o $S3DEST --core-instance-bid-price 0.11 --master-instance-bid-price 0.11 --region eu-west-1
+echo "Now execute these commands to submit the jobs."
+echo "rail-rna go elastic -c 40 -a hg19 -m $MANIFEST -o $S3DEST --core-instance-bid-price 0.11 --master-instance-bid-price 0.11 --region eu-west-1"
 # WITHOUT intron filter
-rail-rna go elastic -c 40 -a hg19 -m $MANIFEST -o $S3DEST.nofilter --core-instance-bid-price 0.11 --master-instance-bid-price 0.11 --region eu-west-1 --intron-criteria 0,0
+echo "rail-rna go elastic -c 40 -a hg19 -m $MANIFEST -o $S3DEST.nofilter --core-instance-bid-price 0.11 --master-instance-bid-price 0.11 --region eu-west-1 --intron-criteria 0,0"
