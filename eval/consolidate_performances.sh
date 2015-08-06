4aaac6d42de5777dbe95bb101c9d6881a33b0503
Abhinav Nellore <anellore@gmail.com>
2015-08-06 00:38:29 -0400

    tweaks scripts for generating intron coverage bias plots

--- eval/consolidate_performances.sh
+++ eval/consolidate_performances.sh
@@ -1,8 +1,12 @@
 #!/usr/bin/env bash
-# Consolidates performances from all sims, local and elastic, in one directory
+# Consolidates performances from sims for all in a single directory "performances" in the same directory as $1
 # $1: directory in which sims were performed
-# $2: directory in which to put performances
-# We ran sh consolidate_performances.sh /scratch0/langmead-fs1/geuvadis_sims_for_paper /scratch0/langmead-fs1/geuvadis_sims_for_paper/performances
+# We ran sh consolidate_performances.sh /scratch0/langmead-fs1/geuvadis_sims_for_paper_v2/8core
+# sh consolidate_performances.sh /dcl01/leek/data/railsims/112simresults
+# sh consolidate_performances.sh /dcl01/leek/data/railsims/8core
+# Then we copied only those files in each "performances" directory the with the sample name NA20768_female_TSI_HMGU_5-1-1 to the local directory
+# /Users/eterna/results_v2, which is inside the SetDirectory of the Mathematica 10 notebook performance.nb
+# You will have to change the argument of SetDirectory there so it matches the directory to which you copy the performance files.
 cd $1
 mkdir -p performances; for i in $(find . -name perform\*); do cp $i performances/${i//\//_}; done
 cd performances
--- eval/harvest_coverages_from_performance.sh
+++ eval/harvest_coverages_from_performance.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 # Uses awk to turn "perform" data output by spliced_read_recovery_performance.py
-# -- as embedded in the script run_all_small_data_sims_locally -- into two matrices:
+# into two matrices:
 # 1) rows = true coverages of introns; columns = (true coverage, # relevant, # retrieved, # relevant and retrieved) instances;
 #	here, an instance is an alignment overlapping introns whose minimally covered intron has coverage = row coverage
 # 2) rows = retrieved coverages of introns; columns = (retrieved coverage # relevant, # retrieved, # relevant and retrieved) instances;
@@ -10,14 +10,15 @@
 TRUETHRESHOLD=100 # max number of rows in matrix 1
 RETRIEVEDTHRESHOLD=100 # max number of rows in matrix 2
 
-INPUT=/scratch0/langmead-fs1/geuvadis_sims_for_paper/performances # where to find output of harvest_spliced_read_performance.py
+INPUT=/Users/eterna/results_v2 # where to find subset of output of harvest_spliced_read_performance.py; could be local
 OUTPUT=$INPUT/harvest
+AWK=/Users/eterna/mawk-1.3.4-20150503/mawk # change this to simply "awk" or download mawk 1.3.4 for a fast implementation of awk
 
 mkdir -p $OUTPUT
 
 for perform in $INPUT/*perform
 do
-	awk '{ 
+	$AWK '{ 
 			for (i = 1; i <= '$TRUETHRESHOLD'; i++) {
 				if ($5 == i) {
 					rel[i] += $1;
@@ -32,7 +33,7 @@ do
 				printf "%d\t%d\t%d\t%d\n",i,rel[i],ret[i],relret[i]
 			}
 		}' $perform >$OUTPUT/$(basename "$perform")_true &
-	awk '{ 
+	$AWK '{ 
 			for (i = 1; i <= '$RETRIEVEDTHRESHOLD'; i++) {
 				if ($8 >= i) {
 					rel[i] += $1;
