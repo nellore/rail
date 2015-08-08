efc96a361dc6804cd1f6a605cf0b86fbb615eb1a
Abhinav Nellore <anellore@gmail.com>
2015-08-08 16:22:29 -0400

    adds dup experiment, updates experiment result tsv

--- eval/prep_28_dup_experiment.sh
+++ eval/prep_28_dup_experiment.sh
@@ -5,4 +5,4 @@ BUCKET=s3://rail-eu-west-1
 # Current dir
 DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 
-rail-rna prep elastic -m ${DIR}/GEUVADIS_28_dup_experiment.manifest -c 20 --core-instance-type c3.2xlarge --master-instance-type c3.2xlarge --core-instance-bid-price 0.11 --master-instance-bid-price 0.11 -o $BUCKET/28duppreprocessed --ec2-key-name raileuw1 --do-not-check-manifest
+rail-rna prep elastic -m ${DIR}/GEUVADIS_28_dup_experiment.manifest -c 20 --core-instance-type c3.2xlarge --master-instance-type c3.2xlarge --core-instance-bid-price 0.11 --master-instance-bid-price 0.11 -o $BUCKET/28dupprep --ec2-key-name raileuw1 --do-not-check-manifest
--- eval/submit_28_dup_experiment.sh
+++ eval/submit_28_dup_experiment.sh
@@ -5,5 +5,5 @@ BUCKET=s3://rail-eu-west-1
 # Get current working directory
 DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 
-rail-rna align elastic -i $BUCKET/28duppreprocessed -a hg19 -o $BUCKET/28dup_out -c 10 --master-instance-type c3.2xlarge --core-instance-type c3.2xlarge -m ${DIR}/GEUVADIS_28_with_112_sample_labels.manifest --ec2-key-name raileuw1 --region eu-west-1 --master-instance-bid-price 0.11 --core-instance-bid-price 0.11
+rail-rna align elastic -i $BUCKET/28dupprep -a hg19 -o $BUCKET/28dupout_v2 -c 40 --master-instance-type c3.2xlarge --core-instance-type c3.2xlarge -m ${DIR}/GEUVADIS_28_dup_experiment.manifest --ec2-key-name raileuw1 --region eu-west-1 --master-instance-bid-price 0.11 --core-instance-bid-price 0.11
 
