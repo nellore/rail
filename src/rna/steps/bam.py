bb37a572766e59b5dace1624ada44b83a3508128
Abhinav Nellore <anellore@gmail.com>
2015-11-21 10:39:16 -0500

    rewrites bam code

--- src/rna/steps/bam.py
+++ src/rna/steps/bam.py
@@ -201,8 +201,6 @@ else:
                         for i in xrange(
                                     len(reference_index.string_to_rname) - 1
                                 )]
-    (output_path, output_filename, output_stream, output_url,
-        last_rname, last_sample_label) = [None]*6
     total_count, unique_count = 0, 0
     if args.out is not None:
         output_url = Url(args.out)
@@ -210,158 +208,202 @@ else:
             # Set up destination directory
             try: os.makedirs(output_url.to_url())
             except: pass
+            output_dir = args.out
         else:
             mover = filemover.FileMover(args=args)
             # Set up temporary destination
             import tempfile
-            temp_dir_path = make_temp_dir(tempdel.silentexpandvars(args.scratch))
+            temp_dir_path = make_temp_dir(
+                                tempdel.silentexpandvars(args.scratch)
+                            )
             register_cleanup(tempdel.remove_temporary_directories,
                                 [temp_dir_path])
-    move_temporary_file = False # True when temporary file should be uploaded
-    while True:
-        line = sys.stdin.readline()
-        if not line:
-            if output_stream is not None:
+            output_dir = temp_dir_path
+
+    from contextlib import contextmanager
+    @contextmanager
+    def stream_and_upload(rnames, filename=None, mover=None, output_url=None
+                            sam=False, samtools_exe=None):
+        """ Yields output stream to write to and uploads as necessary
+
+            sorted_rnames: list of rnames in order of descending length
+            filename: full path to file to write or None if writing to stdout
+            mover: FileMover object or None if no moving should be performed
+            output_url: url to which to write or None if no moving should be
+                performed
+            sam: True iff sam should be output
+            samtools_exe: path to samtools exe if necessary; else None
+
+            Yield value: stream
+        """
+        '''Write SAM header; always include all reference sequences to
+        avoid confusing users.'''
+        header = '\n'.join(
+                ['@HD\tVN:1.0\tSO:coordinate']
+                + [('@SQ\tSN:%s\tLN:%d' % (
+                        header_rname,
+                        reference_index.rname_lengths[header_rname]
+                    )) for header_rname in sorted_rnames]
+                + ['@PG\tID:Rail-RNA\tVN:%s\tCL:%s %s' % (
+                                version.version_number,
+                                sys.executable,
+                                ' '.join(sys.argv)
+                            )]
+            )
+        if filename is None:
+            try:
+                print header
+                yield sys.stdout
+            finally:
+                pass
+        elif sam:
+            try:
+                output_stream = open(filename, 'w')
+                print >>output_stream, header
+                yield output_stream
+            finally:
                 output_stream.close()
-                if not args.output_sam:
-                    samtools_return = samtools_process.wait()
-                    if samtools_return:
-                        raise RuntimeError('samtools returned exitlevel %d' 
-                            % samtools_return)
-                    subprocess_stdout.close()
-                    # Index bam
-                    if not output_path.endswith('.unmapped.bam'):
-                        subprocess.check_call([args.samtools_exe, 'index',
-                                                output_path],
-                                                bufsize=-1)
-            last_output_filename = output_filename
-            last_output_path = output_path
-            move_temporary_file = True
+                if not output_url.is_local:
+                    mover.put(filename, 
+                              output_url.plus(os.path.basename(filename)))
+                    os.remove(filename)
         else:
-            tokens = line.rstrip().split('\t')
-            sample_index, rname_index, pos, qname, flag = tokens[:5]
-            if args.output_by_chromosome:
-                (sample_index, rname_index) \
-                    = sample_and_rname_indexes.sample_and_rname_indexes(
-                            sample_index
+            try:
+                samtools_stream = open(filename, 'wb')
+                samtools_process = subprocess.Popen(
+                            [samtools_exe, 'view', '-bS', '-'],
+                            stdin=subprocess.PIPE,
+                            stdout=samtools_stream
                         )
+                output_stream = samtools_process.stdin
+                print >>output_stream, header
+                yield output_stream
+            finally:
+                output_stream.close()
+                samtools_return = samtools_process.wait()
+                    if samtools_return:
+                        raise RuntimeError(
+                                'samtools returned exit code %d' 
+                                    % samtools_return
+                            )
+                samtools_stream.close()
+                # Index bam
+                unmapped = output_path.endswith('.unmapped.bam')
+                if not unmapped:
+                    subprocess.check_call([samtools_exe, 'index',
+                                            filename],
+                                            bufsize=-1)
+                if not output_url.is_local:
+                    mover.put(filename, 
+                              output_url.plus(os.path.basename(filename)))
+                    if not unmapped:
+                        bai = filename + '.bai'
+                        mover.put(
+                                filename, 
+                                output_url.plus(os.path.basename(bai))
+                            )
+                        os.remove(bai)
+                    os.remove(filename)
+
+    if args.output_by_chromosome:
+        for (index, _), xpartition in xstream(sys.stdin, 2):
+            sample_index, rname_index = (
+                    sample_and_rname_indexes.sample_and_rname_indexes(index)
+                )
             sample_label = manifest_object.index_to_label[sample_index]
             rname = reference_index.string_to_rname[rname_index]
-        if move_temporary_file and last_sample_label is not None \
-            and not output_url.is_local:
-            mover.put(last_output_path, 
-                output_url.plus(last_output_filename))
-            os.remove(last_output_path)
-            if not last_output_path.endswith('.unmapped.bam'):
-                mover.put(
-                    ''.join([last_output_path, '.bai']),
-                    output_url.plus(''.join([last_output_filename, '.bai']))
-                )
-                os.remove(''.join([last_output_path, '.bai']))
-            move_temporary_file = False
-        try:
-            if (sample_label != last_sample_label or rname != last_rname
-                or not line):
-                print 'counts\t-\t%s\t%s\t%d\t%d' % (last_sample_index,
-                                                     last_rname_index,
-                                                     total_count, unique_count)
-                total_count, unique_count = 0, 0
-        except NameError:
-            # First record
-            pass
-        if not line:
-            break
-        if ((sample_label != last_sample_label or 
-                (rname != last_rname and args.output_by_chromosome))
-            and args.out is not None) or output_stream is None:
-            # Create new output file
-            if args.out is not None:
-                if output_stream is not None:
-                    output_stream.close()
-                    if not args.output_sam:
-                        samtools_return = samtools_process.wait()
-                        if samtools_return:
-                            raise RuntimeError('samtools returned exitlevel %d' 
-                                % samtools_return)
-                        subprocess_stdout.close()
-                        # Index bam
-                        if not output_path.endswith('.unmapped.bam'):
-                            subprocess.check_call([args.samtools_exe, 'index',
-                                                    output_path],
-                                                    bufsize=-1)
-                '''If --out is a local file, just write directly to that file.
-                Otherwise, write to a temporary file that will later be
-                uploaded to the destination.'''
-                if rname == '*':
-                    out_rname = 'unmapped'
-                else:
-                    out_rname = rname
-                last_output_filename = output_filename
-                last_output_path = output_path
-                output_filename = args.bam_basename + '.' + sample_label \
-                    + (('.' + out_rname) if args.output_by_chromosome else ''
-                            ) + ('.sam' if args.output_sam else '.bam')
-                if output_url.is_local:
-                    output_path = os.path.join(args.out, output_filename)
-                else:
-                    output_path = os.path.join(temp_dir_path, output_filename)
-                    if last_output_filename is not None:
-                        # Move last output filename iff there is one
-                        move_temporary_file = True
-                output_stream = open(output_path, 'w') if args.output_sam \
-                    else open(output_path, 'wb')
-            else:
-                # Default --out is stdout
-                output_stream = sys.stdout
-                if sys.platform == 'win32' and not args.output_sam:
-                    # Accommodate Windows newline idiosyncrasy
-                    import msvcrt
-                    msvcrt.setmode(sys.stdout.fileno(), os.O_BINARY)
-            if not args.output_sam:
-                # Start samtools and reroute output_stream to subprocess stdin
-                subprocess_stdout = output_stream
-                samtools_process = \
-                    subprocess.Popen([args.samtools_exe, 'view', '-bS', '-'],
-                                        stdin=subprocess.PIPE,
-                                        stdout=subprocess_stdout
-                                    )
-                output_stream = samtools_process.stdin
-            '''Write SAM header; always include all reference sequences to
-            avoid confusing users.'''
-            print >>output_stream, '@HD\tVN:1.0\tSO:coordinate'
-            for header_rname in sorted_rnames:
-                print >>output_stream, '@SQ\tSN:%s\tLN:%d' \
-                    % (header_rname,
-                        reference_index.rname_lengths[header_rname])
-            print >>output_stream, '@PG\tID:Rail-RNA\tVN:%s\tCL:%s %s' % (
-                                    version.version_number,
-                                    sys.executable,
-                                    ' '.join(sys.argv)
-                                )
-        '''Recall that pos has leading 0's so it is sorted properly; remove
-        them below.'''
-        sam_line_to_print = [qname[:254], flag, rname,
-                                str(int(pos))] + [('ZS:i:' + token[5:]
-                                                    if token[:5] == 'XS:i:'
-                                                    else token)
-                                                    for token in tokens[5:]]
-        try:
-            print >>output_stream, '\t'.join(sam_line_to_print)
-        except IOError:
-            raise IOError('Error writing line "%s".' % sam_line_to_print)
-        if not (int(flag) & 256):
-            total_count += 1
-            try:
-                if alignment_printer.unique(sam_line_to_print):
-                    unique_count += 1
-            except IndexError:
-                # Unmapped read; it's unique
-                unique_count += 1
-        last_rname = rname
-        last_sample_label = sample_label
-        last_rname_index = rname_index
-        last_sample_index = sample_index
-        input_line_count += 1
+            unique_count, total_count = 0, 0
+            with stream_and_upload(
+                        sorted_rnames,
+                        filename=(
+                            os.path.join(
+                                    output_dir,
+                                    args.bam_basename + '.' + sample_label
+                                        + ('.unmapped' if rname == '*'
+                                             else ('.' + rname))
+                                        + ('.sam' if args.output_sam
+                                             else '.bam')
+                                ) if args.out is not None else None
+                            ),
+                        mover=(mover if (args.out is not None
+                                         and not output_url.is_local)
+                               else None),
+                        output_url=(None if args.out is None else output_url),
+                        sam=args.output_sam,
+                        samtools_exe=args.samtools_exe
+                    ):
+                for record in xpartition:
+                    sam_line_to_print = [record[1][:254], record[2], rname,
+                                         str(int(record[0]))] + [
+                                            ('ZS:i:' + token[5:]
+                                                if token[:5] == 'XS:i:'
+                                                else token)
+                                                for token in record[3:]]
+                    try:
+                        print >>output_stream, '\t'.join(sam_line_to_print)
+                    except IOError:
+                        raise IOError(
+                                'Error writing line "%s".' % sam_line_to_print
+                            )
+                    if not (int(record[2]) & 256):
+                        total_count += 1
+                        try:
+                            # seq is at position 8
+                            if alignment_printer.unique(record, seq_index=8):
+                                unique_count += 1
+                        except IndexError:
+                            # Unmapped read; it's unique
+                            unique_count += 1
+                    input_line_count += 1
+            # Only primary alignments (flag & 256 != 1)
+            print 'counts\t-\t%s\t%s\t%d\t%d' % (sample_index, rname_index,
+                                                 total_count, unique_count)
+    else:
+        for (sample_index, rname_index), xpartition in xstream(sys.stdin, 2):
+            sample_label = manifest_object.index_to_label[sample_index]
+            rname = reference_index.string_to_rname[rname_index]
+            unique_count, total_count = 0, 0
+            with stream_and_upload(
+                        sorted_rnames,
+                        filename=(
+                            os.path.join(
+                                    output_dir,
+                                    args.bam_basename + '.' + sample_label
+                                        + ('.sam' if args.output_sam
+                                             else '.bam')
+                                ) if args.out is not None else None
+                            ),
+                        mover=(mover if (args.out is not None
+                                         and not output_url.is_local)
+                               else None),
+                        output_url=(None if args.out is None else output_url),
+                        sam=args.output_sam,
+                        samtools_exe=args.samtools_exe
+                    ):
+                for record in xpartition:
+                    sam_line_to_print = [record[1][:254], record[2], rname,
+                                         str(int(record[0]))] + [
+                                            ('ZS:i:' + token[5:]
+                                                if token[:5] == 'XS:i:'
+                                                else token)
+                                                for token in record[3:]]
+                    try:
+                        print >>output_stream, '\t'.join(sam_line_to_print)
+                    except IOError:
+                        raise IOError(
+                                'Error writing line "%s".' % sam_line_to_print
+                            )
+                    if not (int(record[2]) & 256):
+                        total_count += 1
+                        try:
+                            if alignment_printer.unique(record, seq_index=8):
+                                unique_count += 1
+                        except IndexError:
+                            # Unmapped read; it's unique
+                            unique_count += 1
+            # Only primary alignments (flag & 256 != 1)
+            print 'counts\t-\t%s\t%s\t%d\t%d' % (sample_index, rname_index,
+                                                 total_count, unique_count)
 
 print >>sys.stderr, 'DONE with bam.py; in=%d; time=%0.3f s' % (
                                 input_line_count, time.time() - start_time
