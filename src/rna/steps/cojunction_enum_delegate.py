5ddf72c4b26b612656dfa3fb183212b0209681dc
Abhinav Nellore <anellore@gmail.com>
2015-11-26 09:31:29 -0500

    modifies cojunction enum algo

--- src/rna/steps/cojunction_enum_delegate.py
+++ src/rna/steps/cojunction_enum_delegate.py
@@ -26,172 +26,116 @@ from alignment_handlers import multiread_with_junctions, \
 import string
 _reversed_complement_translation_table = string.maketrans('ATCG', 'TAGC')
 
-def maximal_cliques(cojunctions):
-    """ Finds maximal cliques of graph of junction combinations.
+def cojunction_length(cojunction):
+    """ Computes number of exonic bases spanned by cojunction
 
-        Consider an undirected graph where each node corresponds to
-        cojunction, as specified by an element of the list "cojunctions". Place
-        an edge between two nodes for which no two _distinct_ junctions overlap
-        where one junction is associated with one node, and the other junction
-        is associated with the other node. Now enumerate maximal cliques. This
-        gives all possible valid clusters of _consistent_ junctions.
+        cojunction: list of junctions [(intron_start, intron_end, ...), ...]
 
-        This code is adapted from NetworkX's find_cliques(), which requires
-        inclusion of the following copyright notice.
-
-        --------
-        Copyright (C) 2004-2012, NetworkX Developers
-        Aric Hagberg <hagberg@lanl.gov>
-        Dan Schult <dschult@colgate.edu>
-        Pieter Swart <swart@lanl.gov>
-        All rights reserved.
-
-        Redistribution and use in source and binary forms, with or without
-        modification, are permitted provided that the following conditions are
-        met:
-
-          * Redistributions of source code must retain the above copyright
-            notice, this list of conditions and the following disclaimer.
-
-          * Redistributions in binary form must reproduce the above
-            copyright notice, this list of conditions and the following
-            disclaimer in the documentation and/or other materials provided
-            with the distribution.
-
-          * Neither the name of the NetworkX Developers nor the names of its
-            contributors may be used to endorse or promote products derived
-            from this software without specific prior written permission.
-
-
-        THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
-        "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
-        LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
-        A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
-        OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
-        SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
-        LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
-        DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
-        THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
-        (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
-        OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
-        --------
-
-        cojunctions: list of junctions from the same strand
-        
-        Yield value: A maximal clique -- a list of junctions.
+        Return value: number of exonic bases spanned by cojunction
     """
-    # Cache nbrs and find first pivot (highest degree)
-    maxconn=-1
-    nnbrs={}
-    pivotnbrs=set() # handle empty graph
-    for n,nbrs in [(junction, [an_junction for an_junction in cojunctions
-                                if not ((min(an_junction[1], junction[1])
-                                           - max(an_junction[0], junction[0]))
-                                            >= 0)])
-                    for junction in cojunctions]:
-        nbrs=set(nbrs)
-        nbrs.discard(n)
-        conn = len(nbrs)
-        if conn > maxconn:
-            nnbrs[n] = pivotnbrs = nbrs
-            maxconn = conn
-        else:
-            nnbrs[n] = nbrs
-    # Initial setup
-    cand=set(nnbrs)
-    smallcand = set(cand - pivotnbrs)
-    done=set()
-    stack=[]
-    clique_so_far=[]
-    # Start main loop
-    while smallcand or stack:
-        try:
-            # Any nodes left to check?
-            n=smallcand.pop()
-        except KeyError:
-            # back out clique_so_far
-            cand,done,smallcand = stack.pop()
-            clique_so_far.pop()
-            continue
-        # Add next node to clique
-        clique_so_far.append(n)
-        cand.remove(n)
-        done.add(n)
-        nn=nnbrs[n]
-        new_cand = cand & nn
-        new_done = done & nn
-        # check if we have more to search
-        if not new_cand:
-            if not new_done:
-                # Found a clique!
-                yield clique_so_far[:]
-            clique_so_far.pop()
-            continue
-        # Shortcut--only one node left!
-        if not new_done and len(new_cand)==1:
-            yield clique_so_far + list(new_cand)
-            clique_so_far.pop()
-            continue
-        # find pivot node (max connected in cand)
-        # look in done nodes first
-        numb_cand=len(new_cand)
-        maxconndone=-1
-        for n in new_done:
-            cn = new_cand & nnbrs[n]
-            conn=len(cn)
-            if conn > maxconndone:
-                pivotdonenbrs=cn
-                maxconndone=conn
-                if maxconndone==numb_cand:
-                    break
-        # Shortcut--this part of tree already searched
-        if maxconndone == numb_cand:
-            clique_so_far.pop()
-            continue
-        # still finding pivot node
-        # look in cand nodes second
-        maxconn=-1
-        for n in new_cand:
-            cn = new_cand & nnbrs[n]
-            conn=len(cn)
-            if conn > maxconn:
-                pivotnbrs=cn
-                maxconn=conn
-                if maxconn == numb_cand-1:
-                    break
-        # pivot node is max connected in cand from done or cand
-        if maxconndone > maxconn:
-            pivotnbrs = pivotdonenbrs
-        # save search status for later backout
-        stack.append( (cand, done, smallcand) )
-        cand=new_cand
-        done=new_done
-        smallcand = cand - pivotnbrs
+    return sum([cojunction[i][0] - cojunction[i-1][1]
+                    for i in xrange(1, len(cojunction))])
 
-def separated_junctions(junctions, separation):
-    """ Splits junctions up if successive junctions are spaced by > separation
+def paths_from_cojunctions(cojunctions, span=50):
+    """ Finds junction combinations that can be overlapped by span bases
 
-        Two junctions overlapped by readlet alignments may be on the same 
-        strand but much further apart than a read can overlap. This function
-        splits up alignments if successive junctions are separated by more
-        than separation. IT ALSO ENUMERATES ALL JUNCTION COMBINATIONS WITH AND
-        WITHOUT 'EDGE' JUNCTIONS.
+        Consider a directed acyclic graph (DAG) where each node is a different
+        intron (intron_start, intron_end). There is an edge extending from a
+        node A to a node B only if:
+        1) their corresponding introns have no overlapping bases
+        2) A's start and end positions are less than B's start and end
+            positions
+        3) there is no intron C between A and B such that C overlaps neither
+            A nor B
+        Such a graph is constructed from input cojunctions, or junction
+        combinations that may be overlapped by a read. A cojunction is a
+        linear subgraph of the DAG: if it has three junctions, it looks like
+        this---
 
-        alignment_collections: list of junction tuples (pos, end pos)
-        separation: number of bases at or above which junctions should be
-            separated
+           A ---> B ---> C
 
-        Return value: list of lists of junctions.
+        ---for its constituent node A, B, and C. It can be connected to the 
+        rest of the DAG only via its source and/or its sink. All possible
+        edges among sources and sinks of cojunctions are included in the graph.
+
+        Assign a weight to each edge equal to the number of exonic bases
+        between the introns it connects. Now use DFS to enumerate paths
+        through the graph that span at least "span" nodes.
+
+        cojunctions: a list of lists of tuples (intron start, intron end,
+            left displacement, right displacement); each list of tuples
+            represents a different cojunction. EACH COJUNCTION MUST BE 
+            COORDINATE-SORTED. SIDE EFFECT: cojunctions ends
+            up resorted
+        span: the number of exonic bases (total weight) that can be spanned by
+            a path through nodes to return
+
+        Return value: list of junction combinations that can be spanned by
+            span bases; junction combos with "edge" junctions removed are
+            also included
     """
-    if not junctions: return []
-    junctions.sort()
-    prereturn = [[junctions[0]]]
-    for i in xrange(1, len(junctions)):
-        if junctions[i][0] - prereturn[-1][-1][1] >= separation:
-            prereturn.append([junctions[i]])
-        else:
-            prereturn[-1].append(junctions[i])
+    cojunctions_count = len(cojunctions)
+    if cojunctions_count <= 1: return cojunctions
+    '''Make each node of DAG a cojunction; cojunctions are linear subgraphs
+    anyway'''
+    DAG = defaultdict(set)
+    reverse_DAG = defaultdict(set)
+    cojunctions.sort(key=lambda x: (x[0][0], x[-1][1]))
+    for i in xrange(cojunctions_count):
+        source_cojunction_length = cojunction_length(cojunctions[i])
+        for j in xrange(i+1, cojunctions_count):
+            negative_separation = (
+                    min(cojunctions[j][-1][1], cojunctions[i][-1][1])
+                    - max(cojunctions[j][0][0], cojunctions[i][0][0])
+                )
+            if negative_separation >= 0:
+                # Overlap between cojunctions, so continue
+                continue
+            if -negative_separation > span:
+                # Too far away
+                break
+            if (-negative_separation
+                + cojunction_length(cojunctions[j])
+                + source_cojunction_length + 2) <= span:
+                # span can span both cojunctions
+                DAG[i].add(j)
+                reverse_DAG[j].add(i)
+            '''Add edges while there's overlap between jth cojunction and
+            a successive cojunction'''
+            for k in xrange(j+1, cojunctions_count):
+                negative_separation = (
+                        min(cojunctions[k][-1][1], cojunctions[j][-1][1])
+                        - max(cojunctions[k][0][0], cojunctions[j][0][0])
+                    )
+                if negative_separation >= 0:
+                    if (-negative_separation
+                        + cojunction_length(cojunctions[k])
+                        + source_cojunction_length + 2) <= span:
+                        DAG[i].add(k)
+                        reverse_DAG[k].add(i)
+                else:
+                    break
+    sources = set(DAG.keys()) - set(reverse_DAG.keys())
+    sinks = set(reverse_DAG.keys()) - set(DAG.keys())
+    # DFS to enumerate paths
+    paths = []
+    for source in sources:
+        for sink in sinks:
+            if source == sink:
+                paths.append([source])
+                continue
+            stack = [(source, [source])]
+            while stack:
+                (vertex, path) = stack.pop()
+                for next_vertex in DAG[vertex] - set(path):
+                    if next_vertex == sink:
+                        paths.append(path + [next_vertex])
+                    else:
+                        stack.append((next_vertex, path + [next_vertex]))
+    prereturn = [[j for i in [cojunctions[k] for k in path] for j in i]
+                 for path in paths]
     to_return = set()
+    # Add edge combos
     for junction_combo in prereturn:
         to_return.add(tuple(junction_combo[:]))
         to_return.add(tuple(junction_combo[1:-1]))
@@ -243,7 +187,7 @@ def go(input_stream=sys.stdin, output_stream=sys.stdout, fudge=5,
         if flag & 4: continue
         corrected_multiread = multiread_with_junctions(multiread,
                                                         stranded)
-        all_junctions = {}
+        cojunctions, all_junctions = defaultdict(set), {}
         for alignment in multiread_with_junctions(multiread, stranded):
             cigar = alignment[5]
             md = [field for field in alignment
@@ -266,6 +210,10 @@ def go(input_stream=sys.stdin, output_stream=sys.stdout, fudge=5,
             _, _, junctions, _ = indels_junctions_and_exons(
                                                 cigar, md, pos, seq
                                             )
+            cojunctions[(rname, sense)].add(
+                    tuple([(junction[0], junction[1])
+                                for junction in junctions])
+                )
             for junction in junctions:
                 if (junction[0], junction[1]) \
                     not in all_junctions[(rname, sense)]:
@@ -284,41 +232,34 @@ def go(input_stream=sys.stdin, output_stream=sys.stdout, fudge=5,
                             ][1], junction[3])
         for rname, sense in all_junctions:
             to_write = set()
-            # Grab maximal cliques
-            for clique in \
-                maximal_cliques(all_junctions[(rname, sense)].keys()):
-                for cojunctions in separated_junctions(
-                            clique,
-                            separation=(seq_size + fudge)
-                        ):
-                    cojunctions.sort()
-                    left_extend_size = all_junctions[(rname, sense)][
-                                            (cojunctions[0][0],
-                                                cojunctions[0][1])
-                                        ][0]
-                    right_extend_size = all_junctions[(rname, sense)][
-                                             (cojunctions[-1][0],
-                                                cojunctions[-1][1])
-                                         ][1]
-                    to_write.add(('{rname}{sense}\t{starts}'
-                           '\t{ends}\t{left_size}'
-                           '\t{right_size}\t{seq}').format(
-                                rname=rname,
-                                sense=sense,
-                                starts=','.join(
-                                        [str(junction[0])
-                                            for junction in cojunctions]
-                                    ),
-                                ends=','.join(
-                                        [str(junction[1])
-                                            for junction in cojunctions]
-                                    ),
-                                left_size=(left_extend_size
-                                            + fudge),
-                                right_size=(right_extend_size
-                                            + fudge),
-                                seq=seq_to_print
-                           ))
+            for cojunction in paths_from_cojunctions(
+                    list(cojunctions[(rname, sense)]), span=seq_size
+                ):
+                left_extend_size = all_junctions[(rname, sense)][
+                                        cojunction[0]
+                                    ][0]
+                right_extend_size = all_junctions[(rname, sense)][
+                                        cojunction[-1]
+                                    ][1]
+                to_write.add(('{rname}{sense}\t{starts}'
+                       '\t{ends}\t{left_size}'
+                       '\t{right_size}\t{seq}').format(
+                            rname=rname,
+                            sense=sense,
+                            starts=','.join(
+                                    [str(junction[0])
+                                        for junction in cojunction]
+                                ),
+                            ends=','.join(
+                                    [str(junction[1])
+                                        for junction in cojunction]
+                                ),
+                            left_size=(left_extend_size
+                                        + fudge),
+                            right_size=(right_extend_size
+                                        + fudge),
+                            seq=seq_to_print
+                       ))
             for line_to_write in to_write:
                 print line_to_write
                 output_line_count += 1
