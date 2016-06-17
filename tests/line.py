3826e6f8d3f7530f8a37dc56c3e843095c770c66
Abhinav Nellore <anellore@gmail.com>
2016-06-17 16:40:54 -0400

    gzips intermediates in line test

--- tests/line.py
+++ tests/line.py
@@ -418,7 +418,7 @@ if __name__ == '__main__':
         rail_output = subprocess.check_output(
                 [sys.executable, rail_dir, 'go', 'local',
                     '-m', manifest, '-x', bowtie_idx, bowtie2_idx,
-                    '-d', 'tsv,bed,bam,bw'],
+                    '-d', 'tsv,bed,bam,bw', '--gzip-intermediates'],
                 stderr=subprocess.STDOUT
             )
     except subprocess.CalledProcessError as e:
